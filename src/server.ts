2ee7a4a67292c34e75f76af986eccb6922e27a6e
davidleonmayor <jd.leon@unimayor.edu.co>
2025-02-10 17:42:35 -0500

    feat: Cors & allow cookies

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 import express from "express";
-import colors from "colors";
 import morgan from "morgan";
+import cors from "cors";
 
 import budgetRouter from "./routes/budgetRouter";
 import authRouter from "./routes/authRouter";
+import { corsConfig } from "./config/cors";
 
 const app = express();
 
 app.use(morgan("dev"));
 
+// request config
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-// Routes
+// cors
+app.use(cors(corsConfig));
+
+// routes
 app.use("/api/budget", budgetRouter);
 app.use("/api/auth", authRouter);
 
